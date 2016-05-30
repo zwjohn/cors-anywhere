afde75a9566e9ace75b1cfe52ce8f4eadb094e58
zwjohn <zwjohn@yahoo.com>
2016-05-30 12:55:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ var port = process.env.PORT || 8080;
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
-var originBlacklist = (process.env.CORSANYWHERE_BLACKLIST || '').split(',');
+var originBlacklist = ''; //(process.env.CORSANYWHERE_BLACKLIST || '').split(',');
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   //requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: [
-    'cookie',
-    'cookie2',
+    //'cookie',
+    //'cookie2',
     // Strip Heroku-specific headers
-    'x-heroku-queue-wait-time',
-    'x-heroku-queue-depth',
-    'x-heroku-dynos-in-use',
-    'x-request-start',
+   // 'x-heroku-queue-wait-time',
+    //'x-heroku-queue-depth',
+    //'x-heroku-dynos-in-use',
+   // 'x-request-start',
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
